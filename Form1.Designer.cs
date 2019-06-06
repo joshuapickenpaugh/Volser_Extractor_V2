c5bafc86a5c1895fadb8d44e65a7bc1ffbad7642
Joshua Pickenpaugh <joshuapickenpaugh@hotmail.com>
2019-06-06 09:33:23 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Volser_Extractor_V2
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnExtract = new System.Windows.Forms.Button();
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.openFileDialogBox = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(13, 13);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(437, 76);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "BROWSE";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            // 
+            // btnExtract
+            // 
+            this.btnExtract.Location = new System.Drawing.Point(13, 294);
+            this.btnExtract.Name = "btnExtract";
+            this.btnExtract.Size = new System.Drawing.Size(437, 76);
+            this.btnExtract.TabIndex = 1;
+            this.btnExtract.Text = "EXTRACT";
+            this.btnExtract.UseVisualStyleBackColor = true;
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(13, 96);
+            this.txtDisplay.Multiline = true;
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(437, 192);
+            this.txtDisplay.TabIndex = 2;
+            // 
+            // openFileDialogBox
+            // 
+            this.openFileDialogBox.FileName = "openFileDialog1";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 382);
+            this.Controls.Add(this.txtDisplay);
+            this.Controls.Add(this.btnExtract);
+            this.Controls.Add(this.btnBrowse);
+            this.Name = "frmMain";
+            this.Text = "Volser Extractor & File Creator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnExtract;
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.OpenFileDialog openFileDialogBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Via NuGet:
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+//Defined since "Path" was being used "ambigously":
+using Path = System.IO.Path;
+
+namespace Volser_Extractor_V2
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        string strFileContentsAndPathAndName;
+
+        //"BROWSE FOR .PDF FILE" button:
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            // Show the Windows dialog box:
+            DialogResult diagDialogBoxResult = openFileDialogBox.ShowDialog();
+
+            if (diagDialogBoxResult == DialogResult.OK)
+            {
+                strFileContentsAndPathAndName = openFileDialogBox.FileName;
+
+                //Tests for correct ".pdf" suffix:
+                string strFileExtension = Path.GetExtension(strFileContentsAndPathAndName);
+                if (strFileExtension == ".pdf")
+                {
+                    //Displays the filepath and filename in textbox:                    
+                    txtDisplay.Text = strFileContentsAndPathAndName;
+                }
+                //If not correct file type, display messagebox:
+                else
+                {
+                    MessageBox.Show("NOT CORRECT FILE TYPE, PLEASE SELECT YOUR OUTBOUND REQ .PDF FILE");
+                }
+            }
+        }
+
+        //"CREATE FILE" button:
+        private void BtnExtract_Click(object sender, EventArgs e)
+        {
+            //Extract all text from the .pdf:
+            string strText = GetTextFromPDF();
+
+            //...for test poiposes:
+            Console.WriteLine(strText);
+
+            //Function to put strText through a REGEX and return extracted VOLSERS:
+            GetExtractedVolsers(strText);
+
+            //Function to create file and save to same location as original file:
+
+            //Display message to user:
+            MessageBox.Show("Your new VOLSER-extracted file created and placed in " +
+                "same folder as your selected REQ file.");
+
+            //Closes the app:
+            this.Close();
+        }
+
+        //Below code from https://www.c-sharpcorner.com/blogs/reading-contents-from-pdf-word-text-files-in-c-sharp1
+        private string GetTextFromPDF()
+        {
+            StringBuilder text = new StringBuilder();
+            using (PdfReader reader = new PdfReader(strFileContentsAndPathAndName))
+            {
+                for (int i = 1; i <= reader.NumberOfPages; i++)
+                {
+                    text.Append(PdfTextExtractor.GetTextFromPage(reader, i));
+                }
+            }
+            //FOR TEST PURPOSES:
+            //Console.WriteLine(text);
+
+            return text.ToString();
+        }
+        public string GetExtractedVolsers(strText)
+        {
+            string strVolsers = "";
+
+            return strVolsers;
+        }
+    }
+}
